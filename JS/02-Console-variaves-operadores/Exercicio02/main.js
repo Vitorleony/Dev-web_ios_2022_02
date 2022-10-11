3f40fe681563732fc3bd571fdd69d3e4596a992c
Vitorleony <vleony953@gmail.com>
2022-10-10 21:04:28 -0300

    Exercicio  02 JS

---
+++ JS/02-Console-variaves-operadores/Exercicio02/main.js
@@ -0,0 +1,10 @@
+var A = 7;
+
+var B = 8;
+
+var C = 10;
+
+var D = 9;
+
+var media = (A + B + C + D) /4
+console.log("media do aluno é : " + media);
--- JS/02-Console-variaves-operadores/main.js
+++ JS/02-Console-variaves-operadores/main.js
@@ -4,4 +4,4 @@ var b = 'faltou na';
 
 var c = 'sexta-feira';  
 
-console.log(A + B + C);
+console.log(A + B + C)
