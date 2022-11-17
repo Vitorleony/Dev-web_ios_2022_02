75cf50ef5363de7fde435702aaccc8fbe689e7bc
Vitorleony <vleony953@gmail.com>
2022-11-17 11:14:04 -0300

    exercicio12

---
+++ JS/12-Manipulando-DOM-Parte-2/Exercicio-012/main.js
@@ -0,0 +1,41 @@
+function background() {
+    let item1 = document.getElementById('item1');
+    item1.style.backgroundColor = 'blue';
+    console.log(item1);
+  }
+  
+  function font() {
+    let item2 = document.getElementsByClassName('item');
+    item2[1].style.fontWeight = 'bold';
+    console.log(item2);
+  }
+  
+  function zebrado() {
+    let li = document.getElementsByTagName('li');
+    console.log(li);
+    for (let i = 0; i < li.length; i++) {
+        if (i % 2) li[i].style.backgroundColor = 'darkgray';
+        else li[i].style.backgroundColor = 'fff';
+    }
+    console.log(li)
+  }
+  
+  function bottom() {
+    let nome = document.getElementsByName('fitem');
+    nome[0].style.borderBottom = 'solid 5px red';
+    console.log(nome);
+  }
+  
+  let item5 = document.getElementById('item5');
+  
+  function delet(){
+    item5.remove();
+  }
+  
+  let lista = document.getElementById('items');
+  let item4 = document.getElementById('item4');
+  
+  function reset(){
+    lista.insertBefore(item5, item4.nextSibling);
+    item5.textContent = 'Item recuperado';
+  }
