0ef47e3fa47f964c53588f946c1b35439ba6820f
Vitorleony <vleony953@gmail.com>
2022-11-21 21:57:55 -0300

    aula java

---
+++ Java/PrimeiroProjetoJava/src/calcular.java
@@ -0,0 +1,11 @@
+public class calcular {
+    //primeiro passo criar metodo main
+    public static  void  main(String[] args){
+        // Vamos somar as idades de tres alunos
+        //var idade1 = 25
+        int idade1 = 25;
+        int idade2 = 15;
+        int somaidade = idade1 + idade2;
+        System.out.println("A soma da idades dos alunos é:" + somaidade);
+    }
+}
---
+++ Java/PrimeiroProjetoJava/src/pipoca.java
@@ -0,0 +1,9 @@
+public class pipoca {
+    //Mostra no console a frase Hello World
+    public static void main(String[] args){
+    String texto = "Hello World";
+    System.out.println(texto);
+    System.out.print(45);
+    //console.log("Hello World")
+    }
+}
