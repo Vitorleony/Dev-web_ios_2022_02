85ae3be4bdb9941a81958fd241e2c8a1f997c72d
Vitorleony <vleony953@gmail.com>
2022-11-22 21:25:25 -0300

    exercicio

---
+++ Java/03-Tipos-Dados-Operadores/Exercicio1/src/calcularAreaTriangoloRetangulo.java
@@ -0,0 +1,14 @@
+public class calcularAreaTriangoloRetangulo {
+
+    public static void main(String[] args) {
+        int base = 5;
+        int altura = 7;
+        int area = (base * altura) / 2;
+        System.out.println("A área do triangulo é " + area);
+    }
+}
+
+
+
+
+
