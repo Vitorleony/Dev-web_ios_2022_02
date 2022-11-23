f752c137bcb83c64ba936a779e9a699846e521a2
Vitorleony <vleony953@gmail.com>
2022-11-22 21:33:06 -0300

    javas

---
+++ Java/03-Tipos-Dados-Operadores/Exercicio1/src/MediaNota.java
@@ -0,0 +1,12 @@
+public class MediaNota {
+    public static void main(String[] args) {
+        int aluno1 = 10;
+        int aluno2 = 7;
+        int aluno3 = 8;
+        int aluno4 = 9;
+        int calcular = (aluno1 + aluno2 + aluno3 + aluno4) / 4;
+
+        System.out.println("A Média dos alunos é: " + calcular);
+    }
+}
+
